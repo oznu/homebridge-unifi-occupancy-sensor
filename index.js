2263cf1f8edb1d6e22d2e88b7c8f1a8a8f167b09
tickez <tickez@madnet.ch>
2020-01-27 21:36:52 +0100

    Added possibility to limit scope of a device to a particular AP

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-'use strict'
+'use strict';
 
-const debug = require('debug')('unifi')
-const UnifiEvents = require('unifi-events')
-const manifest = require('./package.json')
+const debug = require('debug')('unifi');
+const UnifiEvents = require('unifi-events');
+const manifest = require('./package.json');
 
-var Service, Characteristic
+var Service, Characteristic;
 
 module.exports = function (homebridge) {
-  Service = homebridge.hap.Service
-  Characteristic = homebridge.hap.Characteristic
-  homebridge.registerAccessory('homebridge-unifi-occupancy-sensor', 'UniFi Occupancy Sensor', OccupancySensor)
-}
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory('homebridge-unifi-occupancy-sensor',
+      'UniFi Occupancy Sensor', OccupancySensor)
+};
 
 class OccupancySensor {
-  constructor (log, config) {
-    this.log = log
-    this.name = config.name
-    this.occupancyService = new Service.OccupancySensor(this.name)
-    this.watch = config.watch || []
-    this.watchGuests = config.watchGuests
-    this.mode = config.mode || 'any'
+  constructor(log, config) {
+    this.log = log;
+    this.name = config.name;
+    this.occupancyService = new Service.OccupancySensor(this.name);
+
+    this._watch = config.watch || [];
+    // This allows for backwards compatibility with simple strings
+    this.watch = this._watch.map(function (watched) {
+      if (typeof watched === 'string' || watched instanceof String) {
+        return {"device": watched, "ap": undefined};
+      } else {
+        return watched;
+      }
+    });
+
+    this.watchGuests = config.watchGuests;
+    this.mode = config.mode || 'any';
 
     this.unifi = new UnifiEvents({
       controller: config.unifi.controller,
@@ -28,109 +39,115 @@ class OccupancySensor {
       site: config.unifi.site || 'default',
       rejectUnauthorized: config.unifi.secure || false,
       listen: true
-    })
+    });
 
     this.unifi.on('websocket-status', (socketLog) => {
       this.log(socketLog)
-    })
+    });
 
     this.unifi.on('connected', (data) => {
-      debug(`Device Connected Event Received from UniFi Controller: ${data.msg}`)
+      debug(`Device Connected Event Received from UniFi Controller: ${data.msg}`);
       return this.checkOccupancy()
-    })
+    });
 
     this.unifi.on('disconnected', (data) => {
-      debug(`Device Disconnected Event Received from UniFi Controller: ${data.msg}`)
+      debug(`Device Disconnected Event Received from UniFi Controller: ${data.msg}`);
       return this.checkOccupancy()
-    })
+    });
 
-    this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED
-    this.checkOccupancy()
+    this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED;
+    this.checkOccupancy();
     setInterval(this.checkOccupancy.bind(this), 1800 * 1000)
   }
 
-  checkGuest (isGuest, mac) {
+  checkGuest(isGuest, mac) {
     if (this.watchGuests && isGuest) {
-      debug(`Device [${mac}] is connected to a guest network and guest network monitoring is enabled.`)
+      debug(`Device [${mac}] is connected to a guest network and guest network monitoring is enabled.`);
       return true
     } else if (!this.watchGuests && isGuest) {
-      debug(`Device [${mac}] is connected to a guest network but guest network monitoring is NOT enabled.`)
+      debug(`Device [${mac}] is connected to a guest network but guest network monitoring is NOT enabled.`);
       return false
     } else {
-      debug(`Device [${mac}] is NOT connected to a guest network.`)
+      debug(`Device [${mac}] is NOT connected to a guest network.`);
       return true
     }
   }
 
-  checkOccupancy () {
-    debug('Getting list of connected clients from UniFi Controller...')
-    return this.unifi.getClients()
-      .then((res) => {
-        debug(`${res.data.length} devices are currently connected to the UniFi network, checking each one to see if any are on the watch list...`)
-
-        let activeDevices = res.data.filter((device) => {
-          debug(`Device [${device.mac}] HOSTNAME: "${device.hostname}" , GUEST: "${device.is_guest}", SSID: "${device.essid}"`)
-          if (this.watch.includes(device.mac) && this.checkGuest(device.is_guest, device.mac)) {
-            debug(`Device [${device.mac}] Device is on the watch list. Going to trigger occupancy.`)
-            return true
-          } else {
-            debug(`Device [${device.mac}] Ignoring. Not on the watch list.`)
-            return false
-          }
-        })
-
-        debug(`Monitored devices found:`, activeDevices.map(x => x.mac))
-
-        if (this.mode === 'none') {
-          if (activeDevices.length > 0) {
-            this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "none" so NOT triggering occupancy.`)
-            this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED
-          } else {
-            this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "none" so triggering occupancy.`)
-            this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_DETECTED
-          }
-        } else if (this.mode === 'all') {
-          if (activeDevices.length === this.watch.length) {
-            this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "all" and all watched devices are connected so triggering occupancy.`)
-            this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_DETECTED
-          } else {
-            this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "all" and not all watched devices are connected so NOT triggering occupancy.`)
-            this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED
-          }
-        } else {
-          if (activeDevices.length > 0) {
-            this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "any" so triggering occupancy.`)
-            this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_DETECTED
-          } else {
-            this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "any" so NOT triggering occupancy.`)
-            this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED
-          }
-        }
-
-        this.setOccupancyDetected(this.occupancyDetected)
-      })
-      .catch((err) => {
-        this.log(`ERROR: Failed to check occupancy: ${err.message}`)
-      })
+  isInWatchlist(device) {
+    return this.watch.some(watchedDevice => (
+        watchedDevice.device === device.mac && (watchedDevice.ap === undefined || watchedDevice.ap  === device.ap_mac))
+    );
   }
 
-  getOccupancyDetected (callback) {
+  checkOccupancy() {
+    debug('Getting list of connected clients from UniFi Controller...');
+
+    return this.unifi.getClients()
+    .then((res) => {
+      debug(`${res.data.length} devices are currently connected to the UniFi network, checking each one to see if any are on the watch list...`);
+      let activeDevices = res.data.filter((device) => {
+        debug(`Device [${device.mac}, ${device.ap_mac}] HOSTNAME: "${device.hostname}" , GUEST: "${device.is_guest}", SSID: "${device.essid}"`);
+        if (this.isInWatchlist(device) && this.checkGuest(device.is_guest, device.mac)) {
+          debug(`Device [${device.mac}, ${device.ap_mac}] Device is on the watch list. Going to trigger occupancy.`);
+          return true
+        } else {
+          debug(`Device [${device.mac}] Ignoring. Not on the watch list.`);
+          return false
+        }
+      });
+
+      debug(`Monitored devices found:`, activeDevices.map(x => x.mac));
+
+      if (this.mode === 'none') {
+        if (activeDevices.length > 0) {
+          this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "none" so NOT triggering occupancy.`);
+          this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED
+        } else {
+          this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "none" so triggering occupancy.`);
+          this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_DETECTED
+        }
+      } else if (this.mode === 'all') {
+        if (activeDevices.length === this.watch.length) {
+          this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "all" and all watched devices are connected so triggering occupancy.`);
+          this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_DETECTED
+        } else {
+          this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "all" and not all watched devices are connected so NOT triggering occupancy.`);
+          this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED
+        }
+      } else {
+        if (activeDevices.length > 0) {
+          this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "any" so triggering occupancy.`);
+          this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_DETECTED
+        } else {
+          this.log(`${activeDevices.length} monitored device(s) found. Accessory is in mode "any" so NOT triggering occupancy.`);
+          this.occupancyDetected = Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED
+        }
+      }
+
+      this.setOccupancyDetected(this.occupancyDetected)
+    })
+    .catch((err) => {
+      this.log(`ERROR: Failed to check occupancy: ${err.message}`)
+    })
+  }
+
+  getOccupancyDetected(callback) {
     return callback(null, this.occupancyDetected)
   }
 
-  setOccupancyDetected (value) {
+  setOccupancyDetected(value) {
     return this.occupancyService.setCharacteristic(Characteristic.OccupancyDetected, value)
   }
 
-  getServices () {
+  getServices() {
     var informationService = new Service.AccessoryInformation()
-      .setCharacteristic(Characteristic.Manufacturer, 'oznu')
-      .setCharacteristic(Characteristic.Model, 'unifi-occupancy')
-      .setCharacteristic(Characteristic.SerialNumber, manifest.version)
+    .setCharacteristic(Characteristic.Manufacturer, 'oznu')
+    .setCharacteristic(Characteristic.Model, 'unifi-occupancy')
+    .setCharacteristic(Characteristic.SerialNumber, manifest.version);
 
     this.occupancyService
-      .getCharacteristic(Characteristic.OccupancyDetected)
-      .on('get', this.getOccupancyDetected.bind(this))
+    .getCharacteristic(Characteristic.OccupancyDetected)
+    .on('get', this.getOccupancyDetected.bind(this));
 
     return [informationService, this.occupancyService]
   }
